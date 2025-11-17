6a968bd1a1240a3c25cb867b44e4da929ea8440c
JosephEduard <josepheduardulyloni_2327250080@mhs.mdp.ac.id>
2025-11-18 02:06:10 +0700

    initialize landingpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,12 @@
-import { button as buttonStyles } from "@heroui/theme";
-
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-import PageHead from "@/components/commons/PageHead";
-import { Button } from "@heroui/button";
+import LandingPageLayout from "@/components/LandingPageLayout/LandingPageLayout";
+import Home from "./views/Home";
 // import DefaultLayout from "@/layouts/default";
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <PageHead />
-      <Button color="primary">Click me</Button>
-    </main>
+    <LandingPageLayout title="CV Pandan Sembilan">
+      <Home />
+    </LandingPageLayout>
   );
-}
+};
+export default HomePage;
---
+++ pages/views/Home/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <div>Welcome</div>
+    </div>
+  );
+};
+
+export default Home;
